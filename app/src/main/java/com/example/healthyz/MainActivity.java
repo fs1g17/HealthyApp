87ab9da4a052087a83dfedb631d98c596e0ce5ad
PEEPEEPOOPOO <fs1g17@soton.ac.uk>
2020-03-30 18:28:40 +0100

    Initial commit

---
+++ app/src/main/java/com/example/healthyz/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.healthyz;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private AppBarConfiguration appBarConfiguration;
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationView navView = findViewById(R.id.nav_view);
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        appBarConfiguration =
+                new AppBarConfiguration.Builder(navController.getGraph())
+                        .setDrawerLayout(drawerLayout)
+                        .build();
+        NavigationUI.setupWithNavController(navView, navController);
+        }
+
+}
