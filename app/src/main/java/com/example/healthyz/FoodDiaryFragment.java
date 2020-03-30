f567a8b2243159b6e7dee52ee52237a9e375b827
PEEPEEPOOPOO <fs1g17@soton.ac.uk>
2020-03-30 18:37:54 +0100

    adding some fixes

--- app/src/main/java/com/example/healthyz/FoodDiaryFragment.java
+++ app/src/main/java/com/example/healthyz/FoodDiaryFragment.java
@@ -2,6 +2,8 @@ package com.example.healthyz;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,6 +11,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class FoodDiaryFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
-
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_food_diary, container, false);
+    }
 }
