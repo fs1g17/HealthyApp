732c2ff39cd5436a344fcf4aa8b52d0cae4dafb8
PEEPEEPOOPOO <fs1g17@soton.ac.uk>
2020-04-15 01:48:42 +0100

    modifications in the save() method of the MealRepository class - removed any calls to other methods, thus ensuring that everything is executed on a single thread

--- app/src/main/java/com/example/healthyz/MealRepository.java
+++ app/src/main/java/com/example/healthyz/MealRepository.java
@@ -43,34 +43,37 @@ class MealRepository {
     }
 
     //By calling deleteAll() before saving again, we ensure that deletion works
+    //Removed any calls to other methods - this ensures that everything is done on one thread
     //TODO: fix dates
     void save(@NotNull HashMap<Integer, ArrayList<String>> table, String date){
-        deleteByDate(date);
-        Set<Integer> keySet = table.keySet();
+        MealRoomDatabase.databaseWriteExecutor.execute(() -> {
+            mMealDao.deleteAllByDate(date);
+            Set<Integer> keySet = table.keySet();
 
-        if(!keySet.isEmpty()){
-            for(Integer mealID : keySet){
-                ArrayList<String> foodList = table.get(mealID);
+            if(!keySet.isEmpty()){
+                for(Integer mealID : keySet){
+                    ArrayList<String> foodList = table.get(mealID);
 
-                if(foodList.isEmpty()){
-                    //DO NOTHING
-                }
-                else if(foodList.size() == 1){
-                    Meal meal = new Meal(date,mealID,foodList.get(0));
-                    insert(meal);
-                }
-                else{
-                    String foods = foodList.get(0);
-
-                    for(int i=1; i<foodList.size(); i++){
-                        foods = foods + "\t" + foodList.get(i);
+                    if(foodList.isEmpty()){
+                        //DO NOTHING
                     }
+                    else if(foodList.size() == 1){
+                        Meal meal = new Meal(date,mealID,foodList.get(0));
+                        mMealDao.insert(meal);
+                    }
+                    else{
+                        String foods = foodList.get(0);
 
-                    Meal meal = new Meal(date,mealID,foods);
-                    insert(meal);
+                        for(int i=1; i<foodList.size(); i++){
+                            foods = foods + "\t" + foodList.get(i);
+                        }
+
+                        Meal meal = new Meal(date,mealID,foods);
+                        mMealDao.insert(meal);
+                    }
                 }
             }
-        }
+        });
     }
 }
 
@@ -78,59 +81,3 @@ class MealRepository {
 // on the main thread when the data has changed. We need to not run the insert on the main thread,
 // so we use the ExecutorService we created in the WordRoomDatabase to perform the insert on a
 // background thread.
-
-/*
-*
-List<Meal> loadedMeals = mealsByDate.getValue();
-
-        for(Meal oldMeal : loadedMeals){
-            int mealID = oldMeal.getMealID();
-
-            if(getTableKeySet().contains(mealID)){
-                //CHECK DIFFERENCES
-                String[] oldFoodItems = oldMeal.getMeal().split("\t");
-                ArrayList<String> newFoodItems = table.get(mealID);
-
-                //IF THEY'RE NOT THE SAME SIZE, UPDATE
-                if(oldFoodItems.length != newFoodItems.size()){
-                    mRepository.update(currentDate,mealID,newFoodItems);
-                }
-                else {
-                    boolean update = false;
-
-                    for(String oldFoodItem : oldFoodItems){
-                        if(!newFoodItems.contains(oldFoodItem)){
-                            update = true;
-                            break;
-                        }
-                    }
-
-                    for(String newFoodItem : newFoodItems){
-                        boolean contained = false;
-
-                        for(String oldFoodItem : oldFoodItems){
-                            if(newFoodItem.equals(oldFoodItem)){
-                                contained = true;
-                            }
-                        }
-
-                        if(contained){
-                            update = true;
-                            break;
-                        }
-                    }
-
-                    if(update){
-                        mRepository.update(currentDate,mealID,newFoodItems);
-                    }
-                }
-                //ACHIEVABLE BY INTERSECTION AND UNION;
-
-                //IF THERE ARE DIFFERENCES, UPDATE
-                //IF THERE AREN'T ANY DIFFERENCES, DO NOT UPDATE
-            }
-            else {
-                mRepository.deleteByDateAndMealID(currentDate,mealID);
-            }
-        }
-*/
